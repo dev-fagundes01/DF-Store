7270f18f9b6e94d609bcbe4e4390e245172d13c2
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2024-01-30 20:02:48 -0300

    first commit

---
+++ products.js
@@ -0,0 +1,11 @@
+const products = [
+  { name: "iPhone 11", price: 3799, system: 'IOS', src: "./img/iPhone-11.png" },
+  { name: "Samsung S10", price: 3400, system: 'Android', src: "./img/nb_SAMSUNG-TELEFONO-CELULAR-S10-BLUE.jpeg" },
+  { name: "Samsung Note 9", price: 2500, system: 'Android', src: "./img/celular-samsung-galaxy-note-9-.jpg" },
+  { name: "Redmi POCO x3", price: 1799, system: 'Android', src: "./img/celular-xiaomi-redmi-poco-x3.jpg" },
+  { name: "iPhone 14 Pro", price: 8699, system: 'IOS', src: "./img/14-Pro.jpg" },
+  { name: "LG K10", price: 499, system: 'Android', src: "./img/celular-smartphone-lg-k10-F.jpg" },
+  { name: "Motorola One", price: 1169, system: 'Android', src: "./img/smartphone-motorola-one-xt.jpg" },
+  { name: "iPhone 6s", price: 899, system: 'IOS', src: "./img/celular-apple-iphone-6s.jpg" },
+  { name: "Motorola g9 Power", price: 1199, system: 'Android', src: "./img/celular-samsung-galaxy-note-9-.jpg" }
+];
---
+++ script.js
@@ -0,0 +1,60 @@
+const list = document.querySelector("ul");
+const buttonShowAll = document.querySelector('.show-all')
+const buttonRemove10Percent = document.querySelector('.remove-percent')
+const buttonTotalValue = document.querySelector('.calculate-total-value')
+// const buttonFilterType = document.querySelector('.filter-type')
+const select = document.getElementById('system-select')
+
+function formatCurrency(currency) {
+  return currency.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+}
+
+function showAll(productsArray) {
+  let myLi = ''
+  myLi = ''
+
+  productsArray.forEach((item) => {
+    myLi += `
+      <li>
+        <img src=${item.src} />
+        <p>${item.name}</p>
+        <p class="item-price">${formatCurrency(item.price)}</p>
+      </li> 
+    `;
+  });
+  list.innerHTML = myLi
+}
+
+function remove10Percent() {
+  const newPrices = products.map((item) => ({
+    ...item,
+    price: item.price * 0.9
+  }))
+  showAll(newPrices)
+}
+
+function calculateTotalValue() {
+  const totalValue = products.reduce((acc, item) => { return acc + item.price }, 0)
+
+  list.innerHTML = `
+    <li class="total-value">
+      <p>A soma de todos os itens do menu é: ${formatCurrency(totalValue)}</p>
+    </li>
+  `
+}
+
+function filterType() {
+  if (select.value === 'ios') {
+    const filterBurger = products.filter((item) => item.system === 'IOS')
+    showAll(filterBurger)
+  } else if (select.value === 'android') {
+    const filterBurger = products.filter((item) => item.system === 'Android')
+    showAll(filterBurger)
+  }
+
+}
+
+buttonShowAll.addEventListener('click', () => showAll(products))
+buttonRemove10Percent.addEventListener('click', remove10Percent)
+buttonTotalValue.addEventListener('click', calculateTotalValue)
+select.addEventListener('change', filterType)
